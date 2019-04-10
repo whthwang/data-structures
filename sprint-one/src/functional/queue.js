735f2c3d8063b3a8620b925e9c1771b705c0072a
wayne hwang <whthwang@gmail.com>
2019-04-10 16:12:42 -0700

    completed functional stack and queue exercises. All tests passed.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,40 @@
 var Queue = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
-  var storage = {};
+  // // Use an object with numeric keys to store values
 
-  // Implement the methods below
+  var storage = {};
+  var enqueueNum = 0;
+  var dequeueNum = 0;
+  // // Implement the methods below
+
+  // //collection are the addition of entities to the rear terminal position, known as enqueue, and 
+  // //removal of entities from the front terminal position, known as dequeue
 
   someInstance.enqueue = function(value) {
+    enqueueNum++;
+    storage[enqueueNum] = value;
   };
 
   someInstance.dequeue = function() {
+    if (dequeueNum < enqueueNum) {
+      dequeueNum++;
+      return storage[dequeueNum];
+    }
   };
 
   someInstance.size = function() {
+      return enqueueNum - dequeueNum; 
   };
 
   return someInstance;
+
 };
+
+
+// var newInstance = Queue();
+// newInstance.enqueue(value)
+// newInstance.dequeue()
+
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var stackNum = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    stackNum++;
+    storage[stackNum] = value;
   };
 
   someInstance.pop = function() {
+    if (stackNum > 0) {
+    var poppedOff = storage[stackNum];
+    delete storage[stackNum];
+    stackNum--;
+    return poppedOff;
+    }
   };
 
   someInstance.size = function() {
+    return stackNum;
   };
 
   return someInstance;
