630ca2964b803a230d892e1e21a8892f20dc512b
wayne hwang <whthwang@gmail.com>
2019-04-11 16:21:04 -0700

    committing completed queue.js and stack.js under es6 instantiation pattern no errors.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,25 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(enQNums, deQNums, storage) {
+    this.enQNums = 0;
+    this.deQNums = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.enQNums++;
+    this.storage[this.enQNums] = value;
+  }
+
+  dequeue() {
+    if (this.enQNums > this.deQNums) {
+      this.deQNums++;
+      return this.storage[this.deQNums];
+    }
+  }
+
+  size() {
+    return this.enQNums - this.deQNums;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,29 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, stackNums) {
+    this.storage = {};
+    this.stackNums = 0;
   }
+
+  push(value) {
+    this.stackNums++;
+    this.storage[this.stackNums] = value;
+  }
+
+  pop() {
+    if (this.stackNums > 0) {
+      let poppedOff = this.storage[this.stackNums];
+      delete this.storage[this.stackNums];
+      this.stackNums--;
+      return poppedOff;
+    }
+  }
+
+  size() {
+    return this.stackNums;
+  }
+
+
 
 }
