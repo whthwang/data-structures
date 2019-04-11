4088011f746b7adcd7291b2f4127e668d4a73656
wayne hwang <whthwang@gmail.com>
2019-04-11 11:16:58 -0700

    completed stack.js under prototypal instantitation no errors

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
+//stackMethods = {} and Object.getPrototypeOf(stackMethods) === someObject --> true
+var someObject = Object.create(stackMethods);
+
+  //setting property storage under someObject which is currently an empty object
+  someObject.storage = {};
+  //setting property stackNums under someObject which only has property storage
+  someObject.stackNums = 0;
+
+  return someObject;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+    this.stackNums++;
+    this.storage[this.stackNums] = value;
+  },
+
+  pop: function() {
+    if (this.stackNums > 0) {
+      var poppedOff = this.storage[this.stackNums];
+      delete this.storage[this.stackNums];
+      this.stackNums--;
+      return poppedOff;
+    }
+  },
+
+  size: function() {
+    return this.stackNums;
+  }
+};
+
 
 
